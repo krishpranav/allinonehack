279c2e5d57c26e6f24336144ba837d51f2f07e54
krishpranav <krisna.pranav@gmail.com>
2020-09-05 11:54:46 +0530

    first commit

---
+++ dependencies.py
@@ -0,0 +1,15 @@
+#!usr/bin/env/python
+
+import os
+import time
+
+os.system("apt update")
+os.system("apt install python")
+os.system("pip install json")
+os.system("pip install requests")
+os.system("pip install threading")
+os.system("pip install Queue")
+os.system("pip install glob")
+os.system("pip install httplib")
+os.system("pip install urllib2")
+os.system("pip install base64")
